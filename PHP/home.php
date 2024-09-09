ed6234e30243e3fdac327532830535c2152bbdb7
Taina <taina.kotona02@gmail.com>
2024-09-09 16:52:44 -0300

    Inicío do Projeto

--- PHP/home.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //include('menu.php'); MOSTRA O WARNING QUANDO INSTANCIA COM UMA PÁGINA INEXISTENTE
-    require "menu.php";
-    require_once "menu.php" // IMPEDE REPETIÇÃO
-?>
-
-<hr>
-
-CONTEÚDO DA PÁGINA
